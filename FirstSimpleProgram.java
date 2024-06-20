e030dca5cbc6ddc2190efeb8e5b79e53b2d54854
Manivarman Elumalai <116077118+manivarman30@users.noreply.github.com>
2024-06-20 18:52:36 +0530

    Update FirstSimpleProgram.java

--- FirstSimpleProgram.java
+++ FirstSimpleProgram.java
@@ -1,7 +1,7 @@
 public class FirstSimpleProgram {
    public static void main(String[] args){
 	System.out.println("This is my first Jenkins Build Program");
-
+int i = 10;
 	System.out.println("VELU VELAYUDAM");
 	   
    }
